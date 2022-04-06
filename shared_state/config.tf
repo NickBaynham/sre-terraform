fa4b1f182724355afe5bbb8ee1a92a8e2c0354ac
Nick Baynham <nickbaynham@gmail.com>
2022-04-06 10:09:13 -0400

    Update config.tf
    Updated to match https://github.com/PacktPublishing/Kubernetes-in-Production-Best-Practices/blob/master/Chapter03/terraform/shared-state/config.tf

--- shared_state/config.tf
+++ shared_state/config.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = "~> 0.14.5"	
+  required_version = "~> 0.14.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.31.0"
+    }
+  }
 }
-
-provider = "aws" {
-  region = var.aws_region
-  version = "~> 3.27.0
-}
-
-
