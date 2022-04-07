89c416f4bc7a0e5c1e6c64fdc92d58a896d31298
Nick Baynham <nickbaynham@gmail.com>
2022-04-07 06:57:32 -0400

    Update outputs.tf

--- clusters/outputs.tf
+++ clusters/outputs.tf
@@ -1,1 +1,27 @@
+output "aws_region" {
+  value = var.aws_region
+}
 
+output "cluster_full_name" {
+  value = "${var.clusters_name_prefix}-${terraform.workspace}"
+}
+
+output "cluster_version" {
+  value = var.cluster_version
+}
+
+output "cluster_api" {
+  value = module.nbaynhamcluster.cluster_api
+}
+
+output "cluster_tag" {
+  value = module.nbaynhamcluster.cluster_tag
+}
+
+output "worker_iam_role_arn" {
+  value = module.nbaynhamcluster.worker_iam_role_arn
+}
+
+output "authconfig" {
+  value = module.nbaynhamcluster.authconfig
+}
